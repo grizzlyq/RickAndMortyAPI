8662b124d0954613de82820a0f76872d73e4f849
grizzlyq <pechenika73@yandex.ru>
2022-12-20 18:45:45 +0300

    123

--- MyFirstJSON/App/AppDelegate.swift
+++ MyFirstJSON/App/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UINavigationBar.appearance().tintColor = .white
+//        UINavigationBar.appearance().tintColor = .white
         return true
     }
 
